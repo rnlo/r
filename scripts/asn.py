50ff64023aaf49fc3428e22ff67b19f5e7e2393f
rnlo <rnlo@users.noreply.github.com>
2024-04-19 17:39:05 +0800

    Update

--- scripts/asn.py
+++ scripts/asn.py
@@ -14,16 +14,16 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15'
 }
 
-def scrape_data(region_code):
-    # Define the URL to scrape
-    url = f'https://bgp.he.net/country/{region_code}'
+def get_data_from_url(url, headers):
 
     # Create a session
     with requests.Session() as session:
+        # Update the session headers
         session.headers.update(headers)
 
         # Make the request with the specified headers
         response = session.get(url)
+        # Parse the response text with BeautifulSoup
         soup = BeautifulSoup(response.text, 'html.parser')
 
         # Extract content from tbody table where td[2] is not empty or td[3] is not 0
@@ -36,25 +36,58 @@ def scrape_data(region_code):
             if len(columns) > 2 and columns[0].text.strip().startswith("AS"):
                 if columns[1].text.strip() or columns[2].text.strip() != '0':
                     selected_data.append(columns[0].text.strip().replace("AS", "IP-ASN,"))
+    return selected_data
 
-        # Check if the result contains more than 100 lines, if so, write to file
-        if len(selected_data) > 100:
+def scrape_data(region_code):
 
-            # Write the scraped content to the file with EST timezone timestamp at the beginning
-            eastern = pytz.timezone('US/Eastern')
-            est_time = datetime.now(eastern).strftime('%m/%d/%Y %I:%M:%S %p')
-            region_flag = region_flags.get(region_code.upper(), '')
+    # Define the URL to scrape
+    url = f'https://bgp.he.net/country/{region_code}'
 
-            with open(f'ASN{region_code}.list', 'w') as file:
-                file.write(f"# {region_flag}{region_code}ASN Generated from {url}\n")
-                file.write(f"# Updated at {est_time} (EST)\n")
-                file.write(f"# Total lines: {len(selected_data)}\n")
-                file.write("# https://github.com/rnlo/r\n")
-                file.writelines(f"{line}\n" for line in selected_data)
-            print(f"{region_code}ASN Generated from {url} and saved to ASN{region_code}.list.")
-        else:
-            print(f"Result has less than 100 lines for {region_code}. Not writing to file.")
+    # Get the selected data from the URL
+    selected_data = get_data_from_url(url, headers)
 
-# Define the region code
-scrape_data('US')  # United States
-scrape_data('CN')  # China
+    # Check if the result contains more than 100 lines, if so, write to file
+    if len(selected_data) > 100:
+        # Write the scraped content to the file with EST timezone timestamp at the beginning
+        eastern = pytz.timezone('US/Eastern')
+        est_time = datetime.now(eastern).strftime('%m/%d/%Y %I:%M:%S %p')
+        region_flag = region_flags.get(region_code.upper(), '')
+
+        with open(f'ASN{region_code}.list', 'w') as file:
+            file.write(f"# {region_flag}{region_code}ASN Generated from {url}\n")
+            file.write(f"# Updated at {est_time} (EST)\n")
+            file.write(f"# Total lines: {len(selected_data)}\n")
+            file.write("# https://github.com/rnlo/r\n")
+            file.writelines(f"{line}\n" for line in selected_data)
+        print(f"{region_code}ASN Generated from {url} and saved to ASN{region_code}.list.")
+    else:
+        print(f"Result has less than 100 lines for {region_code}. Not writing to file.")
+    return selected_data
+
+def get_sorted_data(headers):
+    
+    # Define the URL to scrape
+    url = 'https://bgp.he.net/country/CN'
+    
+    # Get the selected data from the URL
+    selected_data = get_data_from_url(url, headers)
+    
+    # Sort the data
+    sorted_data = sorted(selected_data)
+    
+    # Check if the result contains more than 100 lines, if so, write to file
+    if len(sorted_data) > 100:
+        with open('ACN.list', 'w') as file:
+            file.writelines(f"{line}\n" for line in sorted_data)
+        print(f"CNASN Generated from {url} and saved to ACN.list.")
+    else:
+        print("Result has less than 100 lines. Not writing to file.")
+    
+    return sorted_data
+
+if __name__ == "__main__":
+    # Get and sort data from a specific URL
+    get_sorted_data(headers)
+    # Scrape data for specific region codes
+    scrape_data('US')  # United States
+    scrape_data('CN')  # China
--- scripts/rules.py
+++ scripts/rules.py
@@ -1,6 +1,8 @@
-import requests
 import os
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+import pytz
+import requests
 
 # List of file URLs
 file_urls = [
@@ -19,6 +21,9 @@ file_urls = [
     "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Zhihu/Zhihu.list",
     "https://raw.githubusercontent.com/geekdada/surge-list/master/domain-set/dns-filter.txt"
 ]
+
+# List of files to read
+file_names = ["temprule.list", "DouYin.list", "Zhihu.list"]
 
 # Local destination folder path
 destination_folder = os.getcwd()
@@ -53,6 +58,67 @@ def download_file(file_url, new_name):
     else:
         print(f"Unable to retrieve file {file_url}.")
 
-# Use a ThreadPoolExecutor to download the files in parallel
-with ThreadPoolExecutor() as executor:
-    executor.map(download_file, file_urls, new_file_names)
+def download_files_in_parallel(file_urls, new_file_names):
+    # Use a ThreadPoolExecutor to download the files in parallel
+    with ThreadPoolExecutor() as executor:
+        executor.map(download_file, file_urls, new_file_names)
+
+def copy_files_to_temp(file_names, destination_folder):
+    # Define the Eastern Time Zone
+    est = pytz.timezone('US/Eastern')
+
+    # Open or create the "temp.list" file for writing
+    with open("temp.list", "w") as temp_file:
+        for file_name in file_names:
+            # Construct the file path
+            file_path = os.path.join(destination_folder, file_name)
+            
+            if os.path.exists(file_path):
+                with open(file_path, "r") as file:
+                    # Get the current time in EST
+                    est_time = datetime.now(est).strftime("%m/%d/%Y %I:%M:%S %p")
+                    # Append EST timestamp before the file content
+                    temp_file.write(f"# {est_time} (EST)\n")
+                    # Write file content to "temp.list"
+                    temp_file.write(file.read())
+                    temp_file.write("\n")
+                print(f"File copied from '{file_name}' and saved to temp.list.")
+            else:
+                print(f"File '{file_name}' not found.")
+
+def generate_dns_filter():
+
+    url = 'https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_15_DnsFilter/filter.txt'
+    
+    lines_to_exclude = [
+        ".log.netflix.com",
+        ".example1.example.com",
+        ".example2.example.com"
+    ]
+    
+    response = requests.get(url)
+    response.raise_for_status()
+    lines = response.text.splitlines()
+
+    filtered_lines = ["." + line[2:-1] for line in lines if line.startswith("||") and line.endswith("^") and not any(exclude_item in line for exclude_item in lines_to_exclude)]
+    
+    if len(filtered_lines) > 100:
+        # Get current timestamp in Eastern Standard Time (EST)
+        eastern = pytz.timezone('US/Eastern')
+        est_time = datetime.now(eastern).strftime('%H:%M:%S %m/%d/%Y')
+
+        # Write the filtered content to a file with timestamp comments
+        with open(os.path.join(destination_folder, 'dnsfilters.txt'), 'w') as output_file:
+            output_file.write(f"# AdGuard DNS filter for Surge Generated from {url}\n")
+            output_file.write(f"# Updated at EST {est_time}\n")
+            output_file.write(f"# Total lines: {len(filtered_lines)}\n")
+            output_file.write("# https://github.com/rnlo/r\n")
+            output_file.writelines(f"{line}\n" for line in filtered_lines)
+        print(f"AdGuard DNS filter for Surge Generated from {url} and saved to dnsfilters.txt.")
+    else:
+        print("Result has less than 100 lines. Not writing to file.")
+
+if __name__ == "__main__":
+    download_files_in_parallel(file_urls, new_file_names)
+    copy_files_to_temp(file_names, destination_folder)
+    generate_dns_filter()
