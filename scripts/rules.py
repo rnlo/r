05f9d1df4788667fc1116b9cd17e5d057d5ab00e
rnlo <rnlo@users.noreply.github.com>
2024-05-03 19:22:26 +0800

    Update Script

--- scripts/rules.py
+++ scripts/rules.py
@@ -20,6 +20,7 @@ file_urls = {
 filter_parameters = {
     "source_files": {
         "https://raw.githubusercontent.com/LM-Firefly/Rules/master/PROXY/Amazon.list": "Amazon.list",
+        #"https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Amazon/Amazon.list": "AmazonAll.list",
         #"https://raw.githubusercontent.com/LM-Firefly/Rules/master/PROXY/Google.list": "GoogleAll.list"
     },
     "markers": {
@@ -27,10 +28,11 @@ filter_parameters = {
         #"## >> CreateSpace": "## >> End CreateSpace",
         #"## >> Youtube": "## >> End Youtube"
     },
-    "lines_to_exclude": [
+    "lines_to_exclude": {
+        "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/AmazonPrimeVideo/AmazonPrimeVideo.list",
         "example1.example.com",
         "example2.example.com"
-    ]
+    }
 }
 
 # List of files to read
@@ -115,9 +117,29 @@ def download_files_in_parallel(file_urls):
 
 # Download list files and filter from filter_parameters
 def fetch_and_filter_content(filter_parameters):
+    # Fetch lines to exclude from URLs
+    lines_to_exclude_temp = filter_parameters['lines_to_exclude'].copy()
+    for line_to_exclude in lines_to_exclude_temp:
+        if line_to_exclude.startswith('http://') or line_to_exclude.startswith('https://'):
+            try:
+                response = requests.get(line_to_exclude, timeout=10)
+                if response.status_code == 200:
+                    lines = response.text.split('\n')
+                    for line in lines:
+                        if not line.startswith('#'):
+                            filter_parameters['lines_to_exclude'].add(line)
+                filter_parameters['lines_to_exclude'].remove(line_to_exclude)
+            except requests.exceptions.RequestException as e:
+                print(f"An error occurred while fetching the exclude list from '{line_to_exclude}': {e}")
+                print(traceback.format_exc())
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}")
+                print(traceback.format_exc())
+
+    # Fetch and filter source files
     for source_url, filtered_file in filter_parameters['source_files'].items():
         try:
-            response = requests.get(source_url)
+            response = requests.get(source_url, timeout=10)
 
             if response.status_code == 200:
                 content = response.text
