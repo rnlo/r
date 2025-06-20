0ad34d2d2090d255f9ce14097ba5ff01ac822773
rnlo <rnlo@users.noreply.github.com>
2025-06-20 20:20:37 +0800

    Update

--- scripts/rules.py
+++ scripts/rules.py
@@ -94,7 +94,7 @@ file_names_dict = {
         "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Youku/Youku.list",
         "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Xunlei/Xunlei.list",
         "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/CIBN/CIBN.list",
-        "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Alibaba/Alibaba_All_No_Resolve.list",
+        "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Migu/Migu.list",
     ],
 }
 
